c2c93f4ec4b337618b0293d81cbde0841836fe16
SangChan Lee <sangchan82@gmail.com>
2014-08-25 11:38:27 +0900

    Initial Commit

---
+++ Ratings/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ratings
+//
+//  Created by SangChan on 2014. 8. 25..
+//  Copyright (c) 2014년 Spring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Ratings/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ratings
+//
+//  Created by SangChan on 2014. 8. 25..
+//  Copyright (c) 2014년 Spring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
